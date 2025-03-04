8e666172eb87d6c81918badef0d766ec8b9b1dfc
parisisley <pisley2021@fau.edu>
2025-03-04 18:39:59 -0500

    commit html

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function changeText() {
-    document.getElementById("message").innerText = "You clicked the button!";
-}
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio website loaded!");
+
+    // Example: Alert when clicking social media links
+    document.querySelectorAll("a").forEach(link => {
+        link.addEventListener("click", () => {
+            alert(`You are leaving this page to visit ${link.textContent}`);
+        });
+    });
+});
