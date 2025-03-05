01b6f93d82d3a4b14aadcdb67e4a3c80a8f1ecd0
parisisley <pisley2021@fau.edu>
2025-03-04 20:15:51 -0500

    delete

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function toggleServiceInfo(serviceNumber) {
-    const serviceInfo = document.getElementById(`service-info-${serviceNumber}`);
-    if (serviceInfo.style.display === "none" || serviceInfo.style.display === "") {
-        serviceInfo.style.display = "block"; // Show the info
-    } else {
-        serviceInfo.style.display = "none"; // Hide the info
-    }
-}
