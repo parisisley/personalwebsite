2b4dc9573b04a9b8d58b34316c3acd1a658009af
parisisley <pisley2021@fau.edu>
2025-03-05 15:45:41 -0500

    updated config file

--- parisisley/vite.config.js
+++ parisisley/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    port: 3000,
+export default {
+  build: {
+    outDir: 'dist', // Output directory for the build
+    assetsDir: 'assets', // Subdirectory for static assets
+    rollupOptions: {
+      input: 'index.html', // Ensure your entry point is the HTML file
+    },
   },
-});
+};
