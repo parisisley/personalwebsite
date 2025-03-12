677173ec1af5060b0579ac29d1b44e8cf54c0baf
parisisley <pisley2021@fau.edu>
2025-03-12 16:36:11 -0400

    delete

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  build: {
-    outDir: 'dist', // Output directory for the build
-    assetsDir: 'assets', // Subdirectory for static assets
-    rollupOptions: {
-      input: 'index.html', // Ensure your entry point is the HTML file
-    },
-  },
-};
