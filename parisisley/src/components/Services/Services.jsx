4f64907d4da39652d7416587dd49fb7776bb4f74
parisisley <pisley2021@fau.edu>
2025-03-12 12:21:36 -0400

    added pop up windows

--- parisisley/src/components/Services/Services.jsx
+++ parisisley/src/components/Services/Services.jsx
@@ -4,22 +4,18 @@ import './Services.css';
 import webdesign from '../images/webdesign.png'; // Ensure the path is correct
 import printing from '../images/3Dprinting.png';
 import it from '../images/it.png';
+
 function Services() {
   const services = [
     {
       title: '3D Printing',
-      description: 'Building modern, responsive websites.',
+      description: '3D printing services for prototypes, models, and unique designs.',
       imageUrl: printing,
     },
     {
       title: 'Home & Office IT Services',
-      description: 'Designing eye-catching visuals.',
+      description: 'Setup and support for computers, networks, and devices.',
       imageUrl: it,
-    },
-    {
-      title: 'Graphic Design',
-      description: 'Designing eye-catching visuals.',
-      imageUrl: '/path/to/image2.jpg',
     },
     {
       title: 'Web Development',
@@ -31,7 +27,6 @@ function Services() {
       description: 'Designing eye-catching visuals.',
       imageUrl: '/path/to/image2.jpg',
     },
-    // Add more services here
   ];
 
   return (
@@ -45,5 +40,7 @@ function Services() {
     </section>
   );
 }
+
+
 
 export default Services;
