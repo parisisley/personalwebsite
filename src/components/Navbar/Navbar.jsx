6eaf1b63b497cdc5e1be0941f4288c7680bebc89
parisisley <pisley2021@fau.edu>
2025-03-12 16:05:54 -0400

    build

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"
+import "./Navbar.css";
 import eiffel from "../images/eiffel-tower.png";
 
 function Navbar() {
